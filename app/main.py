fee403647c95fad3065e9b47e981ee6a115593fb
Kuai Yu <104323387+C1araYu@users.noreply.github.com>
2024-10-27 18:48:45 -0400

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -6,23 +6,15 @@ from sqlalchemy.future import select  # Import select from sqlalchemy.future
 from app.database import get_db
 from app.models import Job
 from app.schemas import JobCreate
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 app = FastAPI()
-
-@app.post("/jobs/")
-async def create_job(job: JobCreate, db: AsyncSession = Depends(get_db)):
-    new_job = Job(title=job.title, location=job.location)
-    db.add(new_job)
-    await db.commit()
-    await db.refresh(new_job)
-    return new_job
-
-@app.get("/jobs/")
-async def read_jobs(db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(Job))
-    jobs = result.scalars().all()
-    return jobs
 
 @app.get("/")
 async def read_root():
